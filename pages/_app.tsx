7cae49e9b22cc7fafcfe6d694e1c5921cf947491
unknown <elgatobanban@gmail.com>
2025-10-04 00:57:33 -0500

    refactor: _app, to no share layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,39 @@
-import { SessionProvider } from 'next-auth/react';
-import type { AppProps } from 'next/app';
-import '../styles/global.css';
-import Layout from '../components/Layout';
+import { SessionProvider, useSession } from "next-auth/react";
+import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import "../styles/global.css";
+import Layout from "../components/Layout";
 
+function AppContent({ Component, pageProps }: { Component: AppProps["Component"]; pageProps: any }) {
+  const { data: session, status } = useSession({ required: false });
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.replace("/auth/signin");
+    }
+  }, [status, router]);
+
+  if (status === "loading") {
+    return <p className="p-6">Cargando...</p>;
+  }
+
+  if (!session) {
+    return null;
+  }
+
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
 
 export default function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session} basePath="/api/auth">
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <AppContent Component={Component} pageProps={pageProps} />
     </SessionProvider>
   );
 }
